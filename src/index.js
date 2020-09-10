f4f844ddceff4936be583d84d31ad50991ef6d4c
thnamm <nam.tran@journeyh.io>
2020-09-10 17:41:33 +0700

    Modify root files (index)

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import App from "./components/App";
+// import App from "./components/App";
+import Header from "./components/common/Header/Header"
+import Footer from "./components/common/Footer/Footer"
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Header />
+    <Footer />
   </Router>,
   document.getElementById("root")
 );
