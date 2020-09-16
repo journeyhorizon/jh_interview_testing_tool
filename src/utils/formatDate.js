cc3d838ee5324ad6207cbcf47ce506eabb5009d7
thnamm <nam.tran@journeyh.io>
2020-09-16 16:18:35 +0700

    Modify function formatDate for better render UI

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,3 +1,39 @@
+function getWeekday(date) {
+  switch (date) {
+    case 0: return "Sun";
+    case 1: return "Mon";
+    case 2: return "Tue";
+    case 3: return "Wed";
+    case 4: return "Thu";
+    case 5: return "Fri";
+    case 6: return "Sat";
+    default: return "";
+  }
+}
+
+function getDay(date) {
+  if (date < 10) return `0${date}`;
+  else return `${date}`;
+}
+
+function getMonth(date) {
+  switch (date) {
+    case 0: return "01";
+    case 1: return "02";
+    case 2: return "03";
+    case 3: return "04";
+    case 4: return "05";
+    case 5: return "06";
+    case 6: return "07";
+    case 7: return "08";
+    case 8: return "09";
+    case 9: return "10";
+    case 10: return "11";
+    case 11: return "12";
+    default: return "";
+  }
+}
+
 export function formatDate(date) {
   const ago = new Date() - new Date(date);
 
@@ -8,7 +44,8 @@ export function formatDate(date) {
   const seconds = totalSeconds % 60;
 
   if (days > 7) {
-    return new Date(date).toDateString()
+    const submitDate = new Date(date);
+    return `${getWeekday(submitDate.getDay())} ${getDay(submitDate.getDate())}.${getMonth(submitDate.getMonth())}.${submitDate.getFullYear()}`;
   } else if (days === 0) {
     if (hours === 0) {
       if (minutes === 0) {
