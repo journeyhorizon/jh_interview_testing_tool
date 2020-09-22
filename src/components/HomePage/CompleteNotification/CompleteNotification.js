f6c5534111d7858f58eadc178d2a3dea6cff0432
thnamm <nam.tran@journeyh.io>
2020-09-22 10:25:05 +0700

    Create UI and some interactions for components

--- src/components/HomePage/CompleteNotification/CompleteNotification.js
+++ src/components/HomePage/CompleteNotification/CompleteNotification.js
@@ -1,9 +1,37 @@
 import React from "react";
+import * as css from "./CompleteNotification.module.scss";
+
+// Assets
+import logoColor from "../../../assets/logo-color.png";
 
 class CompleteNotification extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isTestCompleted: false
+    }
+  }
+  componentDidMount() {
+    if (!localStorage.getItem("interviewee") || !localStorage.getItem("result"))
+      this.props.history.push("/testlist");
+    else {
+      this.setState({ isTestCompleted: true });
+      console.log("Call API for saving interviewee and its result into database");
+    }
+  }
+
+  componentWillUnmount() {
+    this.state.isTestCompleted && localStorage.clear();
+  }
+
   render() {
     return (
-      <h1>CompleteNotification</h1>
+      <div className={css.container}>
+        <img className={css.titleImage} src={logoColor} alt="logo-company" />
+        <h1 className={css.message}>You have done the Interview Testing!</h1>
+        <p className={css.message}>Congratulating! See you again!</p>
+      </div>
     )
   }
 }
