2897d1a4629607ad472161c49b5a56aebd7c5e84
thnamm <nam.tran@journeyh.io>
2020-09-23 15:54:02 +0700

    Move admin components to common components

--- src/components/common/MobileNavbar/MobileNavbar.js
+++ src/components/common/MobileNavbar/MobileNavbar.js
@@ -2,16 +2,19 @@ import React from "react";
 import * as css from "./MobileNavbar.module.scss";
 
 // Assets
-import menu from "../../../../assets/icons/menu.png";
+import menu from "../../../assets/icons/menu.png";
 
 class MobileNavbar extends React.Component {
   render() {
+    const currentQA = this.props.currentQA;
+    const storage = this.props.storage;
+
     return (
       <div className={css.mobileNavbar}>
         <div className={css.menuButton} onClick={this.props.showMenuBar}>
           <img src={menu} alt="menu" />
         </div>
-        <div className={css.orderOfCurrentPage}><span>{this.props.currentQA.id + 1 || 0}</span>/{this.props.resultOfTest.answerList && this.props.resultOfTest.answerList.length}</div>
+        <div className={css.orderOfCurrentPage}><span>{currentQA.id + 1 || 0}</span>/{storage.answerList && storage.answerList.length}</div>
       </div>
     )
   }
